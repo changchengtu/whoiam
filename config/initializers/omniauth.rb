91b49cf6022dd1f05396ca16b480da910799513b
root <root@ctm135042.ctm.nthu.edu.tw>
2012-12-27 11:47:49 +0800

    facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['458525447538950'], ENV['dccba52e35f292ac0ee578d145c5e7b9']
+  provider :facebook, '458525447538950', 'dccba52e35f292ac0ee578d145c5e7b9'
 end
