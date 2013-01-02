f887ce2b10447cc3ea08ba59446fc8b7cf6d2b44
SOA2012-101078515 <changcheng.tu@gmail.com>
2013-01-02 19:34:44 +0800

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   	protect_from_forgery
-	private	
-    
-  		def current_user
-    			@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  		end
+	
+   	before_filter :current_user 
+	protected
+  	def current_user
+    		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		redirect_to 'auth/:provider/callback' and return unless @current_user
+  	end
   	helper_method :current_user
 end
