406e814d25e240b11a12792261749d33835e8a5f
SOA2012-101078515 <changcheng.tu@gmail.com>
2013-01-02 21:42:10 +0800

    test

--- db/migrate/20121229130319_create_bookideas.rb
+++ db/migrate/20121229130319_create_bookideas.rb
@@ -2,6 +2,7 @@ class CreateBookideas < ActiveRecord::Migration
   def change
     create_table :bookideas do |t|
       t.string :idea
+      t.integer :process
 
       t.references :user
       t.references :book
