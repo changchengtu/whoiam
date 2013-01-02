e8b20fec6beffe3b892c2265eabc00599aa00419
root <root@ctm135042.ctm.nthu.edu.tw>
2013-01-03 01:46:51 +0800

    test

--- app/controllers/mybook_stream_controller.rb
+++ app/controllers/mybook_stream_controller.rb
@@ -1,7 +1,7 @@
 class MybookStreamController < ApplicationController
 
 	def index
-		@tests = User.all
+		@tests = Readedbook.all
 		@books = Book.select(:book_name).all
 		@ideas = Bookidea.select(:book_id).all
 	end
