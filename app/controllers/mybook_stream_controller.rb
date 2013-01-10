2af78b068a67da3d6c1072056f85aeb8d103fc0e
SOA2012-101078515 <changcheng.tu@gmail.com>
2013-01-10 14:11:16 +0800

    test

--- app/controllers/mybook_stream_controller.rb
+++ app/controllers/mybook_stream_controller.rb
@@ -2,6 +2,7 @@ class MybookStreamController < ApplicationController
 	
 	def index
 		@readedbooks = User.find(current_user.id).readed_books.all
+		@test = Uder.find(current_user.id).readed_books.select(:book_name)
 		@ideas = Bookidea.all
 	end
 
